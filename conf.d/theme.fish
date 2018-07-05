90d025679ca03678cd42089c99ee0fbd0b145dff
Dror Levin <spatz@psybear.com>
2018-07-05 15:42:19 +0300

    Enable colors for man pages

--- conf.d/theme.fish
+++ conf.d/theme.fish
@@ -24,3 +24,12 @@ set -U fish_pager_color_completion
 set -U fish_pager_color_description 'B3A06D'  'yellow'
 set -U fish_pager_color_prefix 'white'  '--bold'  '--underline'
 set -U fish_pager_color_progress 'brwhite'  '--background=cyan'
+
+# Enable colors for man pages, adapted from /etc/profile.d/less_termcap.sh
+set -x LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
+set -x LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
+set -x LESS_TERMCAP_me \e'[0m'           # end mode
+set -x LESS_TERMCAP_se \e'[0m'           # end standout-mode
+set -x LESS_TERMCAP_so \e'[01;47;30m'    # begin standout-mode - info box
+set -x LESS_TERMCAP_ue \e'[0m'           # end underline
+set -x LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
