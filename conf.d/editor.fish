6f685391ccff6be1fab01fd9ccf9187b26ab7b54
Roey Darwish Dror <roey.ghost@gmail.com>
2018-07-08 19:52:54 +0300

    Better handling for EDITOR
    ALTERNATE_EDITOR is handled by emacsclient. If it isn't present then we need to use something else

---
+++ conf.d/editor.fish
@@ -0,0 +1,10 @@
+if type -q nvim
+    set -x EDITOR 'nvim'
+else
+    set -x EDITOR 'vim'
+end
+
+if type -q emacsclient
+    set -x ALTERNATE_EDITOR $EDITOR
+    set -x EDITOR 'emacsclient -c'
+end
