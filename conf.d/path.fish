20abde168d69ffe1e45845283f450e0a71dd90e1
Dror Levin <spatz@psybear.com>
2018-09-06 16:02:51 +0300

    Remove gem bin from PATH

--- conf.d/path.fish
+++ conf.d/path.fish
@@ -1,4 +1,4 @@
 set -U fish_user_paths
-for path in /usr/local/opt/coreutils/libexec/gnubin ~/.gem/ruby/*/bin ~/.cargo/bin ~/.local/bin
+for path in /usr/local/opt/coreutils/libexec/gnubin ~/.cargo/bin ~/.local/bin
     test -d "$path"; and set -U fish_user_paths "$path" $fish_user_paths
 end
