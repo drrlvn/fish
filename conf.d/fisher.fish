ddbc572d49138a1d4791c9cea8d628dd0d01efa3
Roey Darwish Dror <roey.ghost@gmail.com>
2018-10-07 11:45:16 +0300

    Migrate to Fisher 3
    Instead of commiting fisher to this repository, we now use the method recommended by fisher to
    provide auto installation. Topgrade will launch `fisher self-update` in order to keep fisher itself
    up to date

--- conf.d/fisher.fish
+++ conf.d/fisher.fish
@@ -1,9 +1,17 @@
 set fisher_file ~/.config/fish/fishfile
-set fish_path ~/.config/fish-plugins
+set -g fisher_path ~/.config/fish-plugins
 
-set fish_function_path $fish_path/functions $fish_function_path
-set fish_complete_path $fish_path/completions $fish_complete_path
+set fish_function_path $fisher_path/functions $fish_function_path
+set fish_complete_path $fisher_path/completions $fish_complete_path
 
-for file in $fish_path/conf.d/*.fish
+for file in $fisher_path/conf.d/*.fish
     builtin source $file 2> /dev/null
+end
+
+if not functions -q fisher
+    echo "Installing fisher for the first time..." >&2
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    source $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fisher
 end
