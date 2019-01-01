e0d9de5088f54b1c48c0a67b74d4f2bab95c77ce
Dror Levin <spatz@psybear.com>
2019-01-01 09:53:33 +0200

    Fixes for fisher 3.2.1

--- conf.d/fisher.fish
+++ conf.d/fisher.fish
@@ -1,8 +1,7 @@
-set fisher_file ~/.config/fish/fishfile
-set -g fisher_path ~/.config/fish-plugins
+set -g fisher_path ~/.config/fish/fish-plugins
 
-set fish_function_path $fisher_path/functions $fish_function_path
-set fish_complete_path $fisher_path/completions $fish_complete_path
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
 
 for file in $fisher_path/conf.d/*.fish
     builtin source $file 2> /dev/null
@@ -12,6 +11,5 @@ if not functions -q fisher
     echo "Installing fisher for the first time..." >&2
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    source $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fisher
+    fish -c fisher
 end
