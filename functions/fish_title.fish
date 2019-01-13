0acb6b3649b86e78ddf234b23797287cc3ca1883
Dror Levin <spatz@psybear.com>
2019-01-13 11:24:11 +0200

    Ignore fish_variables, replace $_ with (status current-command), fixes #6

--- functions/fish_title.fish
+++ functions/fish_title.fish
@@ -1,10 +1,11 @@
 function fish_title
-    switch $_
+    set current_command (status current-command)
+    switch $current_command
         case fish
             prompt_pwd
         case sudo
             echo (string split -m 2 ' ' $argv[1])[2]
         case '*'
-            echo $_
+            echo $current_command
     end
 end
