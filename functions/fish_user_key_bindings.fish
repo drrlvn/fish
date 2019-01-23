540217878d6b40bee31921e50d51c23d2af9473b
Roey Darwish Dror <roey.ghost@gmail.com>
2019-01-23 15:07:37 +0200

    No need to call fzf_key_bindings
    The plugin sets its own key bindings

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -3,6 +3,4 @@ function fish_user_key_bindings
     bind \e\; copy_prev_shell_word
     bind \e\[1\;5C nextd-or-forward-word
     bind \e\[1\;5D prevd-or-backward-word
-
-    type -q fzf_key_bindings; and fzf_key_bindings
 end
