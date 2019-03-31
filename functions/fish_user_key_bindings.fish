86bbd10caad01c80f0e4c420f2c8f62d9b72d563
Roey Darwish Dror <roey.ghost@gmail.com>
2019-03-31 11:21:55 +0300

    fixup! Add a binding for accepting the autosuggestion and execute

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -3,5 +3,5 @@ function fish_user_key_bindings
     bind \e\; copy_prev_shell_word
     bind \e\[1\;5C nextd-or-forward-word
     bind \e\[1\;5D prevd-or-backward-word
-    bind \cx accept-autosuggestion execute
+    bind \e\r accept-autosuggestion execute
 end
