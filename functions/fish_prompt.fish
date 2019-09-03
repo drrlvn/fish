8ac380ff6df200e60d46e9646c5de84b7f523b56
Dror Levin <spatz@psybear.com>
2019-09-03 11:42:01 +0300

    Use starship from PATH, add configuration

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt
     set -l exit_code $status
     set -l starship_duration (math --scale=0 "$CMD_DURATION / 1000")
-    "/usr/bin/starship" prompt --status=$exit_code --cmd-duration=$starship_duration --jobs=(count (jobs -p))
+    starship prompt --status=$exit_code --cmd-duration=$starship_duration --jobs=(count (jobs -p))
 end
