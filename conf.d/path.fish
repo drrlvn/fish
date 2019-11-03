88b8a2ebcaadec90132eccfd9bfbe65334dd92e1
Roey Darwish Dror <roey.ghost@gmail.com>
2019-11-03 10:27:08 +0200

    Make sure that `python` points to Python 3 on Mac (#29)
    Homebrew allows control to what the `python` binary points to. It does this by placing the
    appropriate symlinks in `/usr/local/opt/python/libexec/bin`

--- conf.d/path.fish
+++ conf.d/path.fish
@@ -1,4 +1,4 @@
 set -U fish_user_paths
-for path in /usr/local/opt/coreutils/libexec/gnubin ~/.cargo/bin ~/.yarn/bin ~/.local/bin
+for path in /usr/local/opt/python/libexec/bin /usr/local/opt/coreutils/libexec/gnubin ~/.cargo/bin ~/.yarn/bin ~/.local/bin
     test -d "$path"; and set -U fish_user_paths "$path" $fish_user_paths
 end
