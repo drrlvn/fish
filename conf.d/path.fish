13189be03fa29350a8f57f8ee7a1d89f6360fa0a
Roey Darwish Dror <roey.ghost@gmail.com>
2019-11-06 13:22:50 +0200

    The the PATH directly in Fish
    According the documentation it's not recommended to set fish_user_paths in the configuration. It is
    intended to be set by commands.
    
    https://fishshell.com/docs/current/tutorial.html#tut_path

--- conf.d/path.fish
+++ conf.d/path.fish
@@ -1,4 +1,3 @@
-set -U fish_user_paths
 for path in /usr/local/opt/python/libexec/bin /usr/local/opt/coreutils/libexec/gnubin ~/.cargo/bin ~/.yarn/bin ~/.local/bin
-    test -d "$path"; and set -U fish_user_paths "$path" $fish_user_paths
+    test -d "$path"; and set -x PATH "$path" $PATH
 end
