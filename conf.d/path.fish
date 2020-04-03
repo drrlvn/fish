3b96ed56062b2f799889a63ded56879e6a77f347
Dror Levin <spatz@psybear.com>
2020-04-04 02:52:35 +0300

    Add curl from brew to $PATH on mac

--- conf.d/path.fish
+++ conf.d/path.fish
@@ -1,3 +1,3 @@
-for path in /usr/local/opt/python/libexec/bin /usr/local/opt/coreutils/libexec/gnubin ~/.cargo/bin ~/.yarn/bin ~/.emacs.d/bin ~/.local/bin
+for path in /usr/local/opt/python/libexec/bin /usr/local/opt/coreutils/libexec/gnubin /usr/local/opt/curl-openssl/bin ~/.cargo/bin ~/.yarn/bin ~/.emacs.d/bin ~/.local/bin
     test -d "$path"; and set -x PATH "$path" $PATH
 end
