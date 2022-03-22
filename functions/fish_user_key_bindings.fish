54e51fa7347fc97175b85dc20cf4f83d9f45925f
Dror Levin <dror@psybear.com>
2022-03-22 11:05:07 +0100

    Disable local IP in starship, use doas instead of sudo

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -3,4 +3,5 @@ function fish_user_key_bindings
     bind \e\[1\;5C nextd-or-forward-word
     bind \e\[1\;5D prevd-or-backward-word
     bind \e\r accept-autosuggestion execute
+    bind \es 'fish_commandline_prepend doas'
 end
