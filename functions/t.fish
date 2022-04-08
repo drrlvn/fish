b5cd98155818c86febd0ece80360e55fb95e96c3
Dror Levin <dror@psybear.com>
2022-04-08 12:23:36 +0200

    Fix t function

--- functions/t.fish
+++ functions/t.fish
@@ -1,5 +1,5 @@
 function t -d "Attach to a tmux session if exists, or create it otherwise. If no arguments were given, the session will be called main"
-    if set -q $argv[1]
+    if not set -q argv[1]
         set session_name main
     else
         set session_name $argv[1]
