5afbdeaef9d19089c9e143c7e1b9dc574a918636
Dror Levin <dror@psybear.com>
2023-02-11 11:00:13 +0100

    Go back to using sudo

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -3,5 +3,4 @@ function fish_user_key_bindings
     bind \e\[1\;5C nextd-or-forward-word
     bind \e\[1\;5D prevd-or-backward-word
     bind \e\r accept-autosuggestion execute
-    bind \es 'fish_commandline_prepend doas'
 end
