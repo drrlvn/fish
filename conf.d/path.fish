f8cd6668861c2f6f01a8bdbbd1d5a052ffbcf1d5
Dror Levin <dror@psybear.com>
2023-08-25 02:06:14 +0200

    Minor updates

--- conf.d/path.fish
+++ conf.d/path.fish
@@ -2,6 +2,4 @@ fish_add_path /usr/local/opt/python/libexec/bin
 fish_add_path /usr/local/opt/coreutils/libexec/gnubin
 fish_add_path /usr/local/opt/curl-openssl/bin
 fish_add_path ~/.cargo/bin
-fish_add_path ~/.yarn/bin
-fish_add_path ~/.emacs.d/bin
 fish_add_path ~/.local/bin
