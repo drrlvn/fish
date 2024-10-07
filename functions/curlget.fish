cfd0ad833ee579bea6c16b2ae6d2c83517757be5
Dror Levin <dror@psybear.com>
2024-10-07 16:20:16 +0200

    Remove curlget since curl now has wcurl

--- functions/curlget.fish
+++
@@ -1,3 +0,0 @@
-function curlget -w curl -d 'cURL with wget-like defaults'
-    curl --location --remote-name --remote-time --fail --retry 10 --retry-max-time 10 --continue-at - --progress-bar $argv
-end
